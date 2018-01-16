866a02363a828ed25b9aa7ab1f8e71e6db1bbae6
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2018-01-15 20:32:20 -0500

    Remove Unused methods
    Set thread Name

--- src/main/java/com/gamebuster19901/minejoy/mouse/MouseRunnable.java
+++ src/main/java/com/gamebuster19901/minejoy/mouse/MouseRunnable.java
@@ -3,8 +3,6 @@ package com.gamebuster19901.minejoy.mouse;
 import org.lwjgl.input.Mouse;
 
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.GuiScreenEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public enum MouseRunnable implements Runnable{
 	INSTANCE;
@@ -13,14 +11,10 @@ public enum MouseRunnable implements Runnable{
 	private volatile int mouseTime = 3000;
 	volatile boolean grab = true;
 	volatile boolean shouldSwitch = true;
-	
-	@SubscribeEvent
-	public void onDraw(GuiScreenEvent.DrawScreenEvent.Post e) {
-
-	}
 
 	@Override
 	public void run() {
+		Thread.currentThread().setName("Mouse Thread (MineJoy)");
 		while(true) {
 			try {
 				Thread.sleep(1);
