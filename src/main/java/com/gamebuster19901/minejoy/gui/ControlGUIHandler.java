d05408715c579b24776722317d50da452bedf7f4
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2018-03-13 15:57:59 -0400

    Use I18n instead of TextComponentTranslation

--- src/main/java/com/gamebuster19901/minejoy/gui/ControlGUIHandler.java
+++ src/main/java/com/gamebuster19901/minejoy/gui/ControlGUIHandler.java
@@ -4,7 +4,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiControls;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.client.event.GuiScreenEvent.ActionPerformedEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -17,13 +16,13 @@ public enum ControlGUIHandler {
 		if(e.getGui() instanceof GuiControls) {
 			for(int i = e.getButtonList().size() - 1; i > 0; i--) {
 				GuiButton b = e.getButtonList().get(i);
-				if(b.displayString.contains(new TextComponentTranslation("options.sensitivity").getUnformattedComponentText())) {
+				if(b.displayString.contains(I18n.format("options.sensitivity"))) {
 					e.getButtonList().remove(i);
-					e.getButtonList().add(i, new GuiButton(i, e.getGui().width / 2 - 155 + i % 2 * 160, 18 + 24 * (i - 3), 150, 20, new TextComponentTranslation("options.mouse").getUnformattedComponentText()));
+					e.getButtonList().add(i, new GuiButton(i, e.getGui().width / 2 - 155 + i % 2 * 160, 18 + 24 * (i - 3), 150, 20, I18n.format("options.mouse")));
 				}
-				else if (b.displayString.contains(new TextComponentTranslation("options.invertMouse").getUnformattedComponentText())) {
+				else if (b.displayString.contains(I18n.format("options.invertMouse"))) {
 					e.getButtonList().remove(i);
-					e.getButtonList().add(i, new GuiButton(i, e.getGui().width / 2 - 155 + i % 2 * 160, 18 + 24 * (i - 2), 150, 20, new TextComponentTranslation("options.joy").getUnformattedComponentText()));
+					e.getButtonList().add(i, new GuiButton(i, e.getGui().width / 2 - 155 + i % 2 * 160, 18 + 24 * (i - 2), 150, 20, I18n.format("options.joy")));
 				}
 			}
 		}
@@ -31,12 +30,12 @@ public enum ControlGUIHandler {
 	
 	@SubscribeEvent
 	public void onControlsPress(ActionPerformedEvent.Pre e) {
-		if(e.getButton().displayString.equals(new TextComponentTranslation("options.joy").getUnformattedComponentText())) {
+		if(e.getButton().displayString.equals(I18n.format("options.joy"))) {
 			e.setCanceled(true);
 			e.getButton().playPressSound(Minecraft.getMinecraft().getSoundHandler());
 			Minecraft.getMinecraft().displayGuiScreen(new GuiControllerOptions(Minecraft.getMinecraft().currentScreen, I18n.format("options.choosejoy")));
 		}
-		else if (e.getButton().displayString.equals(new TextComponentTranslation("options.mouse").getUnformattedComponentText())) {
+		else if (e.getButton().displayString.equals(I18n.format("options.mouse"))) {
 			e.setCanceled(true);
 			e.getButton().playPressSound(Minecraft.getMinecraft().getSoundHandler());
 			Minecraft.getMinecraft().displayGuiScreen(new GuiMouseOptions());
